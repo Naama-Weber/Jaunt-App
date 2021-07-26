763fbc84f558e5482b1d1d2f8ae0f39631eb9a78
yotam-bp <yotyyybp@gmail.com>
2021-07-26 13:55:53 +0300

    homepage responsive

--- frontend/src/cmps/BecomeHost.jsx
+++ frontend/src/cmps/BecomeHost.jsx
@@ -3,7 +3,9 @@ import host from '../assets/img/become-a-host.jpg'
 export function BecomeHost() {
     return (
         <div className="host">
+            <div className="img-wrapper">
             <img src={host} alt="" />
+            </div>
             <div className="details flex column justify-center">
                 <h1>Become a Host</h1>
                 <p>Earn extra income and unlock new opportunities by sharing your space.</p>
